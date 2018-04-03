153dbc3ed045d6c13b698d9c04544f5b3316a9f2
frank <188105274@qq.com>
2018-04-03 16:47:09 +0800

    docs路径

--- src/router/blog-routes.js
+++ src/router/blog-routes.js
@@ -1,18 +1,17 @@
-export default [
-  {
-    path: '',
-    redirect: {
-      name: 'BlogList'
+export default [{
+        path: '/docs',
+        redirect: {
+            name: 'BlogList'
+        }
+    },
+    {
+        path: '/docs/BlogList',
+        name: 'BlogList',
+        component: resolve => require(['@/views/BlogList.vue'], resolve)
+    },
+    {
+        path: '/docs/BlogDetail/:number',
+        name: 'BlogDetail',
+        component: resolve => require(['@/views/BlogDetail.vue'], resolve)
     }
-  },
-  {
-    path: 'BlogList',
-    name: 'BlogList',
-    component: resolve => require(['@/views/BlogList.vue'], resolve)
-  },
-  {
-    path: 'BlogDetail/:number',
-    name: 'BlogDetail',
-    component: resolve => require(['@/views/BlogDetail.vue'], resolve)
-  }
 ]
