5e9dd3f8b116fd9bbcaeeb4bfdfbff228ee6866a
ru23 <fanqianweb@gmail.com>
2019-11-27 13:57:38 +0800

    删除fe目录

--- frontend/js/animate.js
+++
@@ -1,3 +0,0 @@
-// build time:Fri Nov 15 2019 13:45:27 GMT+0800 (GMT+08:00)
-$(function(){if(mihoConfig.isHome===true&&document.body.clientWidth>700){var e=["pulse","fadeInRight","flipInX","lightSpeedIn","rotateInUpLeft","slideInUp","zoomIn"],a=e.length,n=e[Math.ceil(Math.random()*a)-1];window.sr=ScrollReveal();sr.reveal("#header-row",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}});sr.reveal(".body-wrap > article",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}})}});
-//rebuild by neat 
