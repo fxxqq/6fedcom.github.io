342a6105622f4f25e471e4113d07ad58cad9b34f
qianqian.fan <qianqian.fan@yintech.cn>
2020-05-20 16:54:23 +0800

    修复图片问题

--- frontend/js/animate.js
+++ frontend/js/animate.js
@@ -1,3 +1,3 @@
-// build time:Tue May 19 2020 21:32:00 GMT+0800 (GMT+08:00)
+// build time:Wed May 20 2020 16:52:21 GMT+0800 (GMT+08:00)
 $(function(){if(mihoConfig.isHome===true&&document.body.clientWidth>700){var e=["pulse","fadeInRight","flipInX","lightSpeedIn","rotateInUpLeft","slideInUp","zoomIn"],a=e.length,n=e[Math.ceil(Math.random()*a)-1];window.sr=ScrollReveal();sr.reveal("#header-row",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}});sr.reveal(".body-wrap > article",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}})}});
 //rebuild by neat 
