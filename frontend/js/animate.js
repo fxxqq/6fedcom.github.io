5c80701d7be75121d6199f29664c3a388324fc6f
樊千千 <fanqianweb@gmail.com>
2020-11-22 15:11:25 +0800

    删除无用代码

--- frontend/js/animate.js
+++ frontend/js/animate.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 30 2020 22:10:35 GMT+0800 (GMT+08:00)
+// build time:Sun Nov 22 2020 15:09:43 GMT+0800 (GMT+08:00)
 $(function(){if(mihoConfig.isHome===true&&document.body.clientWidth>700){var e=["pulse","fadeInRight","flipInX","lightSpeedIn","rotateInUpLeft","slideInUp","zoomIn"],a=e.length,n=e[Math.ceil(Math.random()*a)-1];window.sr=ScrollReveal();sr.reveal("#header-row",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}});sr.reveal(".body-wrap > article",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}})}});
 //rebuild by neat 
